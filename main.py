040f9210f9e4322bd4ce25205b8a3b7207f39479
orioninsist <muhammetmuratkurkoglu@gmail.com>
2022-06-10 14:50:45 -0400

    python password generator updated.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import random
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase_letters = uppercase_letters.lower()
+digits = "0123456789"
+symbols = "()[]{},;:.-_/\?+*#"
+upper,lower,nums,syms = True, True, True, True
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+all = ""
+
+if upper:
+    all += uppercase_letters
+    if lower:
+        all += lowercase_letters
+        if nums:
+            all += digits
+            if syms:
+                all += symbols
+        length = 40
+        amount = 30
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+        for x in range(amount):
+            password = "".join(random.sample(all,length))
+            print(password)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
